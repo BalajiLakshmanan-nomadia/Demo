2fe70b4079bd2d58d4f69be2c40770cbb2921f24
balaji <balaji@nomadia-group.com>
2024-10-07 10:33:17 +0530

    new change to main

--- array.java
+++ array.java
@@ -3,6 +3,10 @@ class array{
         int arr[]={1,2,3,4,5};
         for(int i=0;i<arr.length-1;i++){
             System.out.println(arr[i]);
+            System.out.println(arr[i]);
+            System.out.println(arr[i]);
+            System.out.println(arr[i]);
+            System.out.println(arr[i]);
         }
     }
 }
